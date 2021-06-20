c8bc2aad5fb259fd65151d69c4113fd7948ee4bf
Blucious <27786206@qq.com>
2021-06-20 18:34:33 +0800

    http客户端、api常量布置

---
+++ app/src/main/java/org/group9/fooddelivery/context/ApiContants.java
@@ -0,0 +1,55 @@
+package org.group9.fooddelivery.context;
+
+import java.nio.file.Paths;
+
+/**
+ * API相关常量定义
+ */
+public class ApiContants {
+   public static final String server_url = "http://10.0.2.2:8080";
+
+   // 用户相关
+   public static final String api_user_register = "/api/user/register";
+   public static final String api_user_login = "/api/user/login";
+   public static final String api_user_logout = "/api/user/logout";
+   public static final String api_user_isEmailExists = "/api/user/isEmailExists";
+   public static final String api_user_changeProfile = "/api/user/changeProfile";
+   public static final String api_user_changePasswordByCurrentPassword = "/api/user/changePasswordByCurrentPassword";
+
+   // 收货地址相关
+   public static final String api_deliveryAddress_save = "/api/deliveryAddress/save";
+   public static final String api_deliveryAddress_deleteById = "/api/deliveryAddress/deleteById";
+   public static final String api_deliveryAddress_updateById = "/api/deliveryAddress/updateById";
+   public static final String api_deliveryAddress_listByUserId = "/api/deliveryAddress/listByUserId";
+
+   // 产品类别相关
+   public static final String api_productCategory_save = "/api/productCategory/save";
+   public static final String api_productCategory_list = "/api/productCategory/list";
+
+   // 产品相关
+   public static final String api_product_list = "/api/product/list";
+
+   // 购物车相关
+   public static final String api_cart_save = "/api/cart/save";
+   public static final String api_cart_deleteByProductId = "/api/cart/deleteByProductId";
+   public static final String api_cart_deleteAllForCurrentUser = "/api/cart/deleteAllForCurrentUser";
+   public static final String api_cart_updateByProductId = "/api/cart/updateByProductId";
+   public static final String api_cart_listForCurrentUser = "/api/cart/listForCurrentUser";
+   public static final String api_cart_detailForCurrentUser = "/api/cart/detailForCurrentUser";
+
+   // 订单相关
+   public static final String api_order_createOrder = "/api/order/createOrder";
+   public static final String api_order_setOrderCanceledById = "/api/order/setOrderCanceledById";
+   public static final String api_order_setOrderReceivedById = "/api/order/setOrderReceivedById";
+   public static final String api_order_getOrderById = "/api/order/getOrderById";
+   public static final String api_order_listForCurrentUser = "/api/order/listForCurrentUser";
+   public static final String api_order_listOrderItemsById = "/api/order/listOrderItemsById";
+
+   /**
+    * 将路径与服务器url进行拼接，得到完整的路径
+    */
+   public static String concatPath(String path) {
+      return Paths.get(server_url, path).toString();
+   }
+
+}
--- app/src/main/java/org/group9/fooddelivery/context/FoodDeliveryApplication.java
+++ app/src/main/java/org/group9/fooddelivery/context/FoodDeliveryApplication.java
@@ -2,6 +2,14 @@ package org.group9.fooddelivery.context;
 
 import android.app.Application;
 
+import com.franmontiel.persistentcookiejar.PersistentCookieJar;
+import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
+import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
 
 public class FoodDeliveryApplication extends Application {
 
@@ -17,6 +25,32 @@ public class FoodDeliveryApplication extends Application {
 
       instance = this;
 
-      
+      initHttpClient();
+   }
+
+
+   /**
+    * 使应用程序在全局持有一个Http客户端
+    */
+   private OkHttpClient httpClient;
+
+   private void initHttpClient() {
+
+      PersistentCookieJar cookieJar = new PersistentCookieJar(
+         new SetCookieCache(),
+         new SharedPrefsCookiePersistor(this));
+
+      httpClient = new OkHttpClient.Builder()
+         // 设置Cookie罐
+         .cookieJar(cookieJar)
+         // 设置超时时间
+         .connectTimeout(5, TimeUnit.SECONDS)
+         .readTimeout(30, TimeUnit.SECONDS)
+         .writeTimeout(30, TimeUnit.SECONDS)
+         .build();
+   }
+
+   public OkHttpClient getHttpClient() {
+      return httpClient;
    }
 }
