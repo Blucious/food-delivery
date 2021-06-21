d25eb408fdc8084fe398471f9fba50bb002dbc99
she <ac1690470441@gmail.com>
2021-06-21 12:00:31 +0800

    UserInfo page update

--- app/src/main/java/org/group9/fooddelivery/ui/user/UserFragment.java
+++ app/src/main/java/org/group9/fooddelivery/ui/user/UserFragment.java
@@ -1,5 +1,6 @@
 package org.group9.fooddelivery.ui.user;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import org.group9.fooddelivery.databinding.FragmentUserBinding;
+import org.group9.fooddelivery.ui.address.AddressListActivity;
 
 public class UserFragment extends Fragment {
 
@@ -27,6 +29,21 @@ public class UserFragment extends Fragment {
 
 
    private void initView() {
+      bd.addressbutton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            Intent intent = new Intent(getContext(), AddressListActivity.class);
+            startActivity(intent);
+         }
+      });
+
+      bd.updateinfobutton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            Intent intent = new Intent(getContext(), UserInfo.class);
+            startActivity(intent);
+         }
+      });
    }
 
 }
---
+++ app/src/main/java/org/group9/fooddelivery/ui/user/UserInfo.java
@@ -0,0 +1,14 @@
+package org.group9.fooddelivery.ui.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+import org.group9.fooddelivery.R;
+import android.os.Bundle;
+
+public class UserInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
